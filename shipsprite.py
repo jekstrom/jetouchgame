d1a7e6b9bfab53061f41dbaf239074027a8d85f8
rey9990@gmail.com <rey9990@gmail.com@3f0738cc-4a7f-4c16-1a08-876bcb601906>
2012-04-04 21:13:52 +0000

    added shipsprite.py

---
+++ shipsprite.py
@@ -0,0 +1,21 @@
+import sys
+import time
+
+from touch import *
+import pygame
+from pygame.locals import *
+import utils
+from dirty import *
+from oru import *
+from animatedshipsprite import *
+
+images = (pygame.image.load("spaceship1.png"), pygame.image.load("spaceship2.png"), pygame.image.load("spaceship3.png"))
+
+class ShipSprite(AnimatedShipSprite, pygame.sprite.Sprite):
+    image = None
+
+    def __init__(self, initial_pos, pos):
+        AnimatedShipSprite.__init__(self, images, pos)
+
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = initial_pos
