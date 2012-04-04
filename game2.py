d70e75ee1699be823207dae602d8063a5d41251d
rey9990@gmail.com <rey9990@gmail.com@3f0738cc-4a7f-4c16-1a08-876bcb601906>
2012-04-04 21:43:46 +0000

    Refactored main stuff some more

--- game2.py
+++ game2.py
@@ -93,7 +93,10 @@ tu = touch_up(t)
 td = touch_down(t)
 tm = touch_move(t)
        
-def endGame():
+screen = pygame.display.set_mode(sd, pygame.HWSURFACE)
+background = pygame.image.load("background.jpg")
+
+def endGame(bombs):
     if pygame.font:
         font = pygame.font.Font(None,50)
         gameOver = font.render("GAME OVER", 1, (255,0,0))
@@ -101,101 +104,131 @@ def endGame():
         scoreText = font.render("Score: ", 1, (255,0,0))
         global score
         scorePoints = font.render(str(score), 1, (255,0,0))
-        pygame.draw.rect(background, (0,0,0), (text.get_rect().top, 0, text.get_rect().centerx + 75, 30))
+        pygame.draw.rect(background, (0,0,0), (scoreText.get_rect().top, 0, scoreText.get_rect().centerx + 75, 30))
         screen.blit(gameOver, (sd[0]/2 - gameOver.get_rect().centerx, sd[1]/2))
         screen.blit(scoreText, (sd[0]/2 - scoreText.get_rect().centerx, sd[1]/2 + 55))
         screen.blit(scorePoints, (sd[0]/2 - scorePoints.get_rect().centerx + 55, sd[1]/2 + 55))
     for bomb in bombs:
         bomb.kill()
 
-boxes = RenderUpdates()
+def displayMenu():
+    screen = pygame.display.set_mode(sd, pygame.HWSURFACE)
+    background = pygame.image.load("background.jpg")
 
-bombs = RenderUpdates()
+    pygame.display.update()
+    while True:
+        t.update()
+        screen.blit(background, (0,0))
 
-for location in [[sd[0]/2, sd[1]/2]]:
-   boxes.add(ship)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and \
+                                                 event.key == pygame.K_ESCAPE):
+                sys.exit() 
 
-for y in range (0, sd[1] + 50, 50):
-    for location in [[0, y]]:
-        bomb = BombSprite(location, sd)
-        bombs.add(bomb)
+        if pygame.font:
+            font = pygame.font.Font(None,50)
+            title = font.render("Space game", 1, (250,250,250))
+            screen.blit(title, (sd[0]/2 - title.get_rect().centerx, sd[1]/2))
+            
 
-screen = pygame.display.set_mode(sd, pygame.HWSURFACE)
-background = pygame.image.load("background.jpg")
+        pygame.display.flip()
 
-pygame.display.update()
-i=0
-while True:
-    t.update()
-
-    #screen.fill([0,0,0])
-    screen.blit(background, (0,0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and \
-                                             event.key == pygame.K_ESCAPE):
-            sys.exit()          
-
-    if len(boxes) == 0:
-        endGame()
-
-    boxes.update(pygame.time.get_ticks(), pos)
-    bombs.update(ship)
-
-    clock.tick(50)
-    #boxes.clear(screen, background)
-    #bombs.clear(screen, background)
-
-    rectlist = boxes.draw(screen)
-    rectlist2 = bombs.draw(screen)
-
-    pygame.display.update(rectlist)
-    pygame.display.update(rectlist2)
+def startGame():
+    boxes = RenderUpdates()
+    bombs = RenderUpdates()
     
-    if shoot_laser == True and clock_s >= time.time() and clock_s != 0 and len(boxes) != 0:
-        pygame.draw.line(screen, (0,200,0), laser_pos, (ship.getX(), ship.getY()))
-        for bomb in bombs:
-            #Distance of the laserbeam (from ship center to line end
-            #d = math.sqrt(math.pow(ship.getX()-laser_pos[0],2) + math.pow(ship.getY()-laser_pos[1],2))
-            a = math.pow(ship.getX() - laser_pos[0],2) + math.pow(ship.getY() - laser_pos[1],2)
-            b = 2 * ((ship.getX() - laser_pos[0]) * (laser_pos[0] - bomb.getX()) +\
-                         (ship.getY() - laser_pos[1]) * (laser_pos[1] - bomb.getY()))
+    for location in [[sd[0]/2, sd[1]/2]]:
+        boxes.add(ship)
+        
+    #TODO: Randomize adding of bombs
+    for y in range (0, sd[1] + 50, 50):
+        for location in [[0, y]]:
+            bomb = BombSprite(location, sd)
+            bombs.add(bomb)
 
-            c = (math.pow(bomb.getX(),2) + math.pow(bomb.getY(),2) + math.pow(laser_pos[0],2) +\
-                     math.pow(laser_pos[1],2) - 2 * (bomb.getX() * laser_pos[0] + bomb.getY() * laser_pos[1]) -\
-                     math.pow(bomb.getR(),2))
+    screen = pygame.display.set_mode(sd, pygame.HWSURFACE)
+    background = pygame.image.load("background.jpg")
 
-            i = b * b - 4.0 * a * c
-            if i < 0.0:
-                pass #no intersections
-            elif i > 0:
-            #There is an intersection
-                #Make sure that the laser beam does not end before the bomb...
-                if laser_pos[0] < bomb.getX() and ship.getX() < bomb.getX():
-                    pass
-                elif laser_pos[1] < bomb.getY() and ship.getY() < bomb.getY():
-                    pass
-                elif laser_pos[0] > bomb.getX() and ship.getX() > bomb.getX():
-                    pass
-                elif laser_pos[1] > bomb.getY() and ship.getY() > bomb.getY():
-                    pass
-                else:
-                    #the laser beam cuts across the bomb, destroy it.
-                    bomb.kill()
-                    score += 1
-    else:
-        shoot_laser = False
+    #Start main loop
+    pygame.display.update()
+    while True:
+        t.update()
 
-    #render score
-    if pygame.font:
-        font = pygame.font.Font(None,36)
-        text = font.render("Score: ", 1, (0,0,255))
-        global score
-        textScore = font.render(str(score), 1, (0,0,255))
-        pygame.draw.rect(background, (0,0,0), (text.get_rect().top, 0, text.get_rect().centerx + 75, 30))
-        screen.blit(text, (0,0))
-        screen.blit(textScore, (text.get_rect().centerx + 50, 0))
+        screen.blit(background, (0,0))
 
-     
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and \
+                                                 event.key == pygame.K_ESCAPE):
+                sys.exit()          
+                
+        #No more ships exist
+        if len(boxes) == 0:
+            endGame(bombs)
+                    
+        boxes.update(pygame.time.get_ticks(), pos)
+        bombs.update(ship)
+        
+        clock.tick(50)
+        #boxes.clear(screen, background)
+        #bombs.clear(screen, background)
 
-    pygame.display.flip()
+        rectlist = boxes.draw(screen)
+        rectlist2 = bombs.draw(screen)
+
+        pygame.display.update(rectlist)
+        pygame.display.update(rectlist2)
+        
+        #Handle laser beam collisions
+        global shoot_laser
+        if shoot_laser == True and clock_s >= time.time() and clock_s != 0 and len(boxes) != 0:
+            pygame.draw.line(screen, (0,200,0), laser_pos, (ship.getX(), ship.getY()))
+            for bomb in bombs:
+                #Distance of the laserbeam (from ship center to line end
+                #d = math.sqrt(math.pow(ship.getX()-laser_pos[0],2) + math.pow(ship.getY()-laser_pos[1],2))
+                a = math.pow(ship.getX() - laser_pos[0],2) + math.pow(ship.getY() - laser_pos[1],2)
+                b = 2 * ((ship.getX() - laser_pos[0]) * (laser_pos[0] - bomb.getX()) +\
+                             (ship.getY() - laser_pos[1]) * (laser_pos[1] - bomb.getY()))
+
+                c = (math.pow(bomb.getX(),2) + math.pow(bomb.getY(),2) + math.pow(laser_pos[0],2) +\
+                         math.pow(laser_pos[1],2) - 2 * (bomb.getX() * laser_pos[0] + bomb.getY() * laser_pos[1]) -\
+                         math.pow(bomb.getR(),2))
+                
+                i = b * b - 4.0 * a * c
+                if i < 0.0:
+                    pass #no intersections
+                elif i > 0:
+                    #There is an intersection
+                    #Make sure that the laser beam does not end before the bomb...
+                    if laser_pos[0] < bomb.getX() and ship.getX() < bomb.getX():
+                        pass
+                    elif laser_pos[1] < bomb.getY() and ship.getY() < bomb.getY():
+                        pass
+                    elif laser_pos[0] > bomb.getX() and ship.getX() > bomb.getX():
+                        pass
+                    elif laser_pos[1] > bomb.getY() and ship.getY() > bomb.getY():
+                        pass
+                    else:
+                        #the laser beam cuts across the bomb, destroy it.
+                        bomb.kill()
+                        score += 1
+        else:
+            shoot_laser = False
+
+        #render score
+        if pygame.font:
+            font = pygame.font.Font(None,36)
+            text = font.render("Score: ", 1, (0,0,255))
+            global score
+            textScore = font.render(str(score), 1, (0,0,255))
+            pygame.draw.rect(background, (0,0,0), (text.get_rect().top, 0, text.get_rect().centerx + 75, 30))
+            screen.blit(text, (0,0))
+            screen.blit(textScore, (text.get_rect().centerx + 50, 0))        
+
+        pygame.display.flip()
+    
+def main():
+    displayMenu()
+
+if __name__ == "__main__":
+    main()
+    
