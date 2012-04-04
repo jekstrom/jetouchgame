d3a5b0596b899d64a0dff2ca9f74fb975a35feca
rey9990@gmail.com <rey9990@gmail.com@3f0738cc-4a7f-4c16-1a08-876bcb601906>
2012-04-04 21:47:40 +0000

    implemented button sprite

--- buttonsprite.py
+++ buttonsprite.py
@@ -1,2 +1,26 @@
+import sys
+
+from touch import *
+import pygame
+from pygame.locals import *
+
 class Button(pygame.sprite.Sprite):
-    pass
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('button.png', -1)
+
+    def setCoords(self, pos):
+        self.rect.topleft = pos
+
+    def isPressed(self, pos):
+        if pos[0] > self.rect.topleft[0]:
+            if pos[1] > self.rect.topleft[1]:
+                if pos[0] < self.rect.topleft[0]:
+                    if pos[1] < self.rect.topleft[1]:
+                        return true
+                    else: return false
+                else: return false
+            else: return false
+        else: return false
+
+    
